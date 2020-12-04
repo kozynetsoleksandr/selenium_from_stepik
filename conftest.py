ae95e71db7d41d3fbfa0a7f84e558a5c11277095
kozynetsoleksandr <kozynetsoleksandr@gmail.com>
2020-12-05 01:20:57 +0200

    Added a method and handled the exception

--- conftest.py
+++ conftest.py
@@ -4,12 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
+
     parser.addoption('--language', action='store', default='en-gb',
                      help="set language")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
+
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
+
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
@@ -9,6 +10,7 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
+
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
