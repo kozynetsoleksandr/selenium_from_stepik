4f7d47381653375dfc005f03a90a02fc5d312b67
kozynetsoleksandr <kozynetsoleksandr@gmail.com>
2020-12-06 20:11:12 +0200

    Finally commit, I am happy!

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
-from pages.base_page import BasePage
-from pages.locators import *
+from .locators import LoginPageLocators
+from .base_page import BasePage
 
 
 class LoginPage(BasePage):
@@ -9,13 +9,24 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url, "'login' not in current url"
+        # проверка на корректный url адрес
+        result = self.is_element_present('http://selenium1py.pythonanywhere.com/en-gb/accounts/login/')
+        assert result
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Don\'t have login form'
+        # проверка, что есть форма логина зарегистрированного пользователя на странице
+        result = self.is_element_present(*LoginPageLocators.LOGIN_FORM)
+        assert result
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Don\'t have register form'
+        # проверка, что есть форма регистрации на странице
+        result = self.is_element_present(*LoginPageLocators.REG_FORM)
+        assert result
+
+    def register_new_user(self, email, password):
+        # регистрация нового пользователя
+        self.browser.get('http://selenium1py.pythonanywhere.com/en-gb/accounts/login/')
+        self.browser.find_element(*LoginPageLocators.REG_LOGIN).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REG_PASSWORD1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REG_PASSWORD2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.SUBMIT_BUTTON).click()
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,21 @@
+import time
 from .base_page import BasePage
 from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
+    def __init__(self, *args, **kwargs):
+        super(MainPage, self).__init__(*args, **kwargs)
 
-    def go_to_login_page(self):
+    def should_be_browse_store(self):
+        assert self.is_element_present(*MainPageLocators.BROWSE_STORE), "Browse store link is not presented"
 
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)  # Символ * роспаковывает переданный кортеж
-        login_link.click()
-
-    def should_be_login_link(self):
-
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+    def all_products(self):
+        assert self.is_element_present(*MainPageLocators.ALL_BOOKS), "All books list is not presented"
+        text = self.browser.find_element(*MainPageLocators.ALL_BOOKS).get_attribute("innerHTML")
+        all_prod = self.browser.find_element(*MainPageLocators.ALL_BOOKS)
+        all_prod.click()
+        page_title = self.browser.find_element(*MainPageLocators.HEADER_ALLBOOKS)
+        title = page_title.get_attribute("innerHTML")
+        time.sleep(5)
+        assert text in title, "ALL PRODUCTS title is not presented"
